81cd5b61f5223783b3a4ac7ff7fe7b63db47c571
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-06-16 13:43:24 +0530

    design & performance issues

---
+++ week1/transfer-service-1/src/main/java/com/npci/repository/NoSqlAccountRepository.java
@@ -0,0 +1,32 @@
+package com.npci.repository;
+
+import com.npci.model.Account;
+
+/**
+ *
+ * author : Foo team
+ */
+
+public class SqlAccountRepository {
+
+    public SqlAccountRepository(){
+        System.out.println("SqlAccountRepository initialized");
+    }
+
+    public Account loadAccount(String number) {
+        System.out.println("Loading account: " + number);
+        // logic to load account from SQL database
+        Account account = new Account(); // object
+        account.number = number;
+        account.balance = 1000.00; // Example balance
+        return account;
+    }
+
+    public void updateAccount(Account account) {
+        // logic to update account in SQL database
+        // For example, updating the balance
+        System.out.println("Updating account "+ account.number );
+    }
+
+
+}
