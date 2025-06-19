8a1a87a0fc29effcdbc2e0a7f75e65d1ec8cb4a5
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-06-19 13:20:31 +0530

    builder

--- week1/oo-patterns/src/main/java/com/npci/creational/builder/Application.java
+++ week1/oo-patterns/src/main/java/com/npci/creational/builder/Application.java
@@ -1,4 +1,29 @@
 package com.npci.creational.builder;
 
+class Employee {
+    private String firstName; // Required
+    private String lastName; // Required
+    private String email; // Optional
+    private String phoneNumber; // Optional
+    private String address; // Optional
+    private String department; // Optional
+}
+
+
 public class Application {
+    public static void main(String[] args) {
+
+        // firstName and lastName
+
+        // firstName and lastName + email
+
+        // firstName and lastName + email + phoneNumber
+
+        // firstName and lastName + email + phoneNumber + address
+
+        // firstName and lastName + email + address
+
+        // firstName and lastName + email + department
+
+    }
 }
