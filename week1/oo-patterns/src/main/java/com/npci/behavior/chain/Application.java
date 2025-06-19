d25d1fcac9490a3d5f0ff78d3746121d303aa460
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-06-19 14:02:11 +0530

    chain

--- week1/oo-patterns/src/main/java/com/npci/behavior/chain/Application.java
+++ week1/oo-patterns/src/main/java/com/npci/behavior/chain/Application.java
@@ -1,4 +1,29 @@
 package com.npci.behavior.chain;
 
+
+interface Handler {
+    void setNext(Handler next);
+
+    void handleRequest(double amount);
+}
+
+class FiveHundredHandler implements Handler {
+    private Handler nextHandler;
+
+    public void setNext(Handler next) {
+        this.nextHandler = next;
+    }
+
+    public void handleRequest(double amount) {
+        // e,g amount is 1000, then this handler will handle 500
+        // e.g if amount is 1200 , then next handler will be called
+    }
+}
+
 public class Application {
+
+    public static void main(String[] args) {
+
+    }
+
 }
