fe3fb095ce52fe3f969ac778cf1a2392a832d4e7
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-06-23 13:58:11 +0530

    List collection

---
+++ week2/collections/src/main/java/com/npci/Application2.java
@@ -0,0 +1,13 @@
+package com.npci;
+
+import com.npci.util.LinkedList;
+
+public class Application2 {
+    public static void main(String[] args) {
+
+        LinkedList list=new LinkedList();
+        list.add("person1"); // 0
+        list.add("person3"); // 1
+        list.add(1,"person2");
+    }
+}
