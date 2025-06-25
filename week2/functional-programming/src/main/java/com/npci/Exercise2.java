8330bab8f991e6ea81eb1c8c3abd04eef761d78a
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-06-25 11:49:09 +0530

    Why we need stream API

--- week2/functional-programming/src/main/java/com/npci/Exercise2.java
+++ week2/functional-programming/src/main/java/com/npci/Exercise2.java
@@ -3,8 +3,10 @@ package com.npci;
 import com.npci.model.Dish;
 import com.npci.model.DishType;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise2 {
     public static void main(String[] args) {
@@ -21,7 +23,71 @@ public class Exercise2 {
                 new Dish("salmon", false, 450, DishType.FISH));
 
 
-        // Req-1 : get low calorie (calories < 400) dish's names, sorted by calories
+        // Req-1 : get low calorie (calories < 400), sorted by calories asc, dish's names,
+
+        System.out.println(
+//                getLowCalorieDishNames_v1(menu)
+                getLowCalorieDishNames_v2(menu)
+        );
 
     }
+
+    //--------------------------
+    // data processing
+    //--------------------------
+    // filtering
+    // sorting
+    // mapping aka transforming
+    // grouping
+    // partitioning
+    // aggregating
+    // statistics
+    // anyMatch, allMatch, noneMatch
+    // ... and many more
+
+    // data processing pipeline in imperative style
+
+    // problem with imperative style:
+    // -> too much code
+    // -> hard to read
+    // -> hard to parallelize
+    // -> hard to maintain
+    // -> hard to test
+
+    // solution: use functional style programming with Java Streams API
+
+    private static List<String> getLowCalorieDishNames_v1(List<Dish> menu) {
+        // step-1: filter low calorie dishes ( calories < 400 )
+        List<Dish> lowCalorieDishes = new ArrayList<>();
+        for (Dish d : menu) {
+            if (d.getCalories() < 400) {
+                lowCalorieDishes.add(d);
+            }
+        }
+        // step-2: sort low calorie dishes by calories
+        class CaloriesComparator implements java.util.Comparator<Dish> {
+            public int compare(Dish d1, Dish d2) {
+                return Integer.compare(d1.getCalories(), d2.getCalories());
+            }
+        }
+        lowCalorieDishes.sort(new CaloriesComparator());
+        // step-3: get dish names
+        List<String> lowCalorieDishNames = new ArrayList<>();
+        for (Dish d : lowCalorieDishes) {
+            lowCalorieDishNames.add(d.getName());
+        }
+        return lowCalorieDishNames;
+    }
+
+    private static List<String> getLowCalorieDishNames_v2(List<Dish> menu) {
+        return menu
+                .stream()
+                .filter(d -> d.getCalories() < 400)
+                .sorted((d1, d2) -> Integer.compare(d1.getCalories(), d2.getCalories()))
+                .map(Dish::getName)
+                .collect(Collectors.toList());
+    }
+
 }
+
+
