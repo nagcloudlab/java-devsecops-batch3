100332eed2fd4cf072701a2d3f12476930773450
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-06-25 13:57:25 +0530

    Stream Exercise solution

--- week2/functional-programming/src/main/java/com/npci/Exercise3.java
+++ week2/functional-programming/src/main/java/com/npci/Exercise3.java
@@ -29,5 +29,38 @@ public class Exercise3 {
         // Query 5: Are there any trader based in Milan?
         // Query 6: Update all transactions so that the traders from Milan are set to Cambridge.
         //----------------------------------------------------------
+
+        // Query 1: Find all transactions from year 2011 and sort them by value (small to high).
+        transactions.stream()
+                .filter(t -> t.getYear() == 2011)
+                .sorted((t1, t2) -> Integer.compare(t1.getValue(), t2.getValue()))
+                .forEach(System.out::println);
+        // Query 2: What are all the unique cities where the traders work?
+        transactions.stream()
+                .map(txn -> txn.getTrader().getCity())
+                .distinct()
+                .forEach(System.out::println);
+        // Query 3: Find all traders from Cambridge and sort them by name.
+        transactions.stream()
+                .map(txn -> txn.getTrader())
+                .filter(trader -> trader.getCity().equals("Cambridge"))
+                .sorted((t1, t2) -> t1.getName().compareTo(t2.getName()))
+                .forEach(System.out::println);
+        // Query 5: Are there any trader based in Milan?
+        boolean areAnyMilanBased = transactions.stream()
+                .anyMatch(txn -> txn.getTrader().getCity().equals("Milan"));
+        // Query 6: Update all transactions so that the traders from Milan are set to Cambridge.
+        transactions.stream()
+                .filter(txn -> txn.getTrader().getCity().equals("Milan"))
+                .forEach(txn -> {
+                    txn.getTrader().setCity("Cambridge");
+                });
+
+
     }
 }
+
+// How to define data ==> OO concepts
+// How to store data ==> collections
+// How to process data functionally => Lambda + Stream API
+// How to process data concurrently | parallaly => MultiThreading
