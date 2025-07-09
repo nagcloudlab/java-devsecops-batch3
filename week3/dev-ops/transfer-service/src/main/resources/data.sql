e70f47095a7289ebd95362410a97d2b3afd60863
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-07-09 12:23:12 +0530

    week-4

---
+++ week3/dev-ops/transfer-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+
+
+
+insert into accounts (account_number,account_holder_name,balance) values
+('1234567890', 'John Doe', 1000.00),
+('0987654321', 'Jane Smith', 1500.50),
+('1122334455', 'Alice Johnson', 2000.75),
+('5566778899', 'Bob Brown', 300.25),
+('6677889900', 'Charlie White', 450.00);
